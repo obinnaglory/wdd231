7e2bcb99686ace79e444c7529e5527953a6403aa
obinnaglory <yagazieobinna@gmail.com>
2026-03-21 17:04:45 +0100

    check

--- date.js
+++
@@ -1,5 +0,0 @@
-// Current year
-document.getElementById("year").textContent = new Date().getFullYear();
-
-// Last modified
-document.getElementById("lastModified").textContent = "Last Modified: " + document.lastModified;
--- navigation.js
+++
@@ -1,6 +0,0 @@
-const menuButton = document.getElementById("menuButton");
-const navMenu = document.getElementById("navMenu");
-
-menuButton.addEventListener("click", () => {
-    navMenu.classList.toggle("open");
-});
