4fa3db51ce868d250f49800d73e6baf2b9adddad
obinnaglory <yagazieobinna@gmail.com>
2026-03-30 16:20:02 +0100

    love week4

--- chamber/scripts/date.js
+++ chamber/scripts/date.js
@@ -3,3 +3,4 @@ document.getElementById("year").textContent = new Date().getFullYear();
 
 // Last modified
 document.getElementById("lastModified").textContent = "Last Modified: " + document.lastModified;
+document.getElementById("timestamp").value = new Date().toISOString();
--- chamber/scripts/ga.js
+++ chamber/scripts/ga.js
@@ -3,3 +3,5 @@ window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
 gtag('config', 'GA_MEASUREMENT_ID');
+ <button onclick="document.getElementById('bronzeModal').showModal()">Learn More</button>
+ 
